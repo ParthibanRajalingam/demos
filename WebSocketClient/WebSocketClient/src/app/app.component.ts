328ead4475f1e54451671cc381bef5dc2c444e24
ParthibanRajalingam1993 <parthiban.r@thoughtworks.com>
2023-04-04 23:13:49 +0530

    Refactor

--- WebSocketClient/WebSocketClient/src/app/app.component.ts
+++ WebSocketClient/WebSocketClient/src/app/app.component.ts
@@ -8,7 +8,6 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'WebSocketClient';
   stock: any = {};
-  indicator : string = '▲';
 
   private webSocket: WebSocket;
 
